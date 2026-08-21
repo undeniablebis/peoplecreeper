ad7650e78598e63daea01b0d12c23be70a6cf8b8
Bismillah Constantino <constantinobismillah@gmail.com>
2026-08-22 06:07:29 +0800

    Added changes to index and created style.css.

--- functions/page.php
+++ functions/page.php
@@ -7,6 +7,7 @@ function page_start($page_title){
     <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="/peoplecreeper/css/style.css">
         <title><?= htmlspecialchars($page_title)?></title>
     </head>
     <body>
--- index.php
+++ index.php
@@ -12,8 +12,40 @@ require_once "functions/page.php";
 page_start("Welcome to PeopleCreeper");
 
 ?>
-
 <h1>Welcome to landing page!</h1>
+<div class="container">
+    <div class="login">
+        <form action="">
+            <label for="">email</label><br>
+            <input type="email" placeholder="email" required><br>
+            <label for="">password</label><br>
+            <input type="password" name="" id="" placeholder="password"><br>
+            <button>login</button><br><br>
+        </form>
+        <small>No account yet?</small>
+        <button>Register</button>
+
+    </div>
+
+    <div class="about">
+        <h1>Welcome to PeopleCreeper</h1>
+
+        <p>
+            PeopleCreeper is a social networking project inspired by the early
+        days of Facebook and other social platforms.
+        </p> 
+
+        <p>
+            Create your own profile, connect with friends, discover other people, 
+            and build your own network.
+        </p>
+
+        <p>
+            This project is also a personal exercise in learning and developing a 
+            social networking application from the ground up.
+        </p>
+    </div>
+</div>
 
 <?php
 
