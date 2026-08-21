d56a1b682854ec44a49a60034ec5062d9eb4b26e
Bismillah Constantino <constantinobismillah@gmail.com>
2026-08-22 04:26:17 +0800

    Initial commit for PeopleCreeper.

---
+++ functions/page.php
@@ -0,0 +1,22 @@
+<?php
+
+function page_start($page_title){
+    ?>
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title><?= htmlspecialchars($page_title)?></title>
+    </head>
+    <body>
+    <?php
+}
+
+function page_end(){
+    ?>
+    </body>
+    </html>
+    <?php
+}?>
+
---
+++ index.php
@@ -0,0 +1,12 @@
+<?php
+require_once "functions/page.php";
+
+page_start("Welcome to PeopleCreeper");
+
+?>
+
+<h1>Welcome to landing page!</h1>
+
+<?php
+
+page_end();
